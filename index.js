87ef95b9495005feb686d119430aaf8ceef1497f
Francisco Funes <ffunes90@gmail.com>
2022-01-20 13:24:29 -0300

    Params and arguments, positional, arguments object, rest parameters

--- index.js
+++ index.js
@@ -60,9 +60,49 @@ const makeBeer2 = function (qty) {
 // make it easier to understand where their belong in the context of an application
 // less likely to pollute the global namespace
 
+/************************/
+
 // Immediately Invoked Function Expression IIEF
 // By wrapping in parenthesis, we can make it an IIFE and call it immediately with the ()
 //example
 (function () {
   const x = 23;
 })();
+
+/************************/
+
+// Params and Arguments
+
+// Arguments
+
+// Positional
+// when you call the function you need the pass the arguments in the correct order
+
+function makeBreakfast(main, side, drink) {
+  console.log(arguments); // {[Iterator] 0: "🍳", 1: "🥪", 2: "🍺"}
+  return `Breakfast includes ${main}, ${side}, and ${drink}`;
+}
+
+const breakfast = makeBreakfast("🥐", "🥟", "🍺");
+
+console.log(breakfast); // Breakfast includes 🥐, 🥟, and 🍺
+
+// when you have a lot of arguments, you can use the arguments object
+
+function makeLunch(opts) {
+  const { main, side, drink } = opts; // destructuring
+  return `Lunch includes ${main}, ${side}, and ${drink}`;
+}
+
+// with this approach you dont have to worry about the order of the arguments
+// Also make it more flexible for extending your application
+makeLunch({ main: "🍳", side: "🥪", drink: "🍺" });
+
+// Rest Parameters
+function makeDinner(...args) {
+  return `Dinner includes the following items: ${args.join(", ")}`;
+}
+
+// that allows to call the function with multiple position arguments
+// access them as an array inside the function body
+makeDinner("🍳", "🥪", "🍺");
