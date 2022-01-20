b3c8208a12e33b2fa832a1208d114039f0eb1180
Francisco Funes <ffunes90@gmail.com>
2022-01-20 13:05:50 -0300

    difference between function declaration and function expression

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// Declaration, also called definition or statement
+/*
+ * Javascript Functions - What's your function?
+ */
+
+// Function Declaration, also called definition or statement
 
 // choose the correct name to you function is as hard as making jokes about programming
 // you can think a parameter as a variable that a function will access when it is called
@@ -20,3 +24,38 @@ function makeBread(qty) {
 // function call
 const loaves = makeBread(5);
 console.log(loaves); // 🍞🍞🍞🍞🍞
+
+/************************/
+
+// Function Expression
+// using a function as a variable
+// using a anonymous function, a function with a name as a value
+
+const makeBeer = function (qty) {
+  return "🍺".repeat(qty);
+};
+
+// function Declaration is hoisted while the function expression is not
+// This means You can make calls to a function before it is defined
+
+// Here an example, read comments in the code
+
+// Declaration
+
+const loaves2 = makeBread(7); // WORKS!
+
+function makeBread2(qty) {
+  return "🍞".repeat(qty);
+}
+
+const beers2 = makeBeer2(7); // makeBeer2 is not defined
+
+// Function Expression
+const makeBeer2 = function (qty) {
+  return "🍺".repeat(qty);
+};
+
+// most people today use function expressions
+// because there are not hoisted
+// make it easier to understand where their belong in the context of an application
+// less likely to pollute the global namespace
